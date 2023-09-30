2f94e21bd04af87f61bcac3a98dedfe3f4d3f7b3
RaiProjct <111714032+RaiProjct@users.noreply.github.com>
2023-09-30 20:52:41 -0300

    update functions/generate-key.js

--- functions/generate-key.js
+++ functions/generate-key.js
@@ -1,16 +1,33 @@
 const crypto = require('crypto');
+const axios = require('axios');
 
-// Esta seria sua lista ou banco de dados de chaves
-const generatedKeys = new Map();
+const API_TOKEN = 'd5713b391c30067d7df3073a3a4fc42bd8bc67fe';
 
 exports.handler = async function(event, context) {
     const newKey = crypto.randomBytes(16).toString('hex');
-    generatedKeys.set(newKey, { 
-        createdAt: Date.now(),
-        used: false
-    });
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ key: newKey })
-    };
-}
+    const apiUrl = `YOUR_NETLIFY_URL/use-key/${newKey}`; // Substitua YOUR_NETLIFY_URL pelo URL do seu projeto no Netlify
+
+    const customAlias = `RaiHub-${newKey.substring(0, 5)}`; // Criando um alias único
+
+    try {
+        const response = await axios.get(`https://encurta.net/api`, {
+            params: {
+                api: API_TOKEN,
+                url: apiUrl,
+                alias: customAlias,
+                format: 'text',
+                type: 0  // Tipo 0 (sem anúncios)
+            }
+        });
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ shortenedLink: response.data })
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: 'Error generating shortened link.'
+        };
+    }
+};
