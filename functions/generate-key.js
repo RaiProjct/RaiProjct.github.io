3f2f9dfa0d4d41aa3beb898cfe81cdfd2b805bbe
RaiProjct <111714032+RaiProjct@users.noreply.github.com>
2023-09-30 20:59:07 -0300

    update functions/generate-key.js

--- functions/generate-key.js
+++ functions/generate-key.js
@@ -5,7 +5,7 @@ const API_TOKEN = 'd5713b391c30067d7df3073a3a4fc42bd8bc67fe';
 
 exports.handler = async function(event, context) {
     const newKey = crypto.randomBytes(16).toString('hex');
-    const apiUrl = `YOUR_NETLIFY_URL/use-key/${newKey}`; // Substitua YOUR_NETLIFY_URL pelo URL do seu projeto no Netlify
+    const apiUrl = `https://raihub.netlify.app//use-key/${newKey}`; // Substitua YOUR_NETLIFY_URL pelo URL do seu projeto no Netlify
 
     const customAlias = `RaiHub-${newKey.substring(0, 5)}`; // Criando um alias único
 
