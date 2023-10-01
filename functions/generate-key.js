03121177c905a153699cbbc100d702ba9d5fc17a
RaiProjct <111714032+RaiProjct@users.noreply.github.com>
2023-10-01 02:22:24 -0300

    update functions/generate-key.js

--- functions/generate-key.js
+++ functions/generate-key.js
@@ -1,68 +1,48 @@
-const fs = require('fs');
-const crypto = require('crypto');
+const MongoClient = require('mongodb').MongoClient;
 const axios = require('axios');
-const path = require('path');
 
-const API_TOKEN = 'd5713b391c30067d7df3073a3a4fc42bd8bc67fe';
+const username = process.env.DB_USER;
+const password = process.env.DB_PASS;
+const dbName = 'Keys';
+const collectionName = 'RaiHub';
+const uri = `mongodb+srv://${username}:${password}@cluster.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
-exports.handler = async function(event, context) {
-    const clientIp = event.headers['client-ip'];
+const encurtaNetAPI = "https://encurta.net/api?api=d5713b391c30067d7df3073a3a4fc42bd8bc67fe&url=https://raihub.netlify.app/showkey&alias=RaiHub&type=0";
 
-    // Carrega as chaves do arquivo JSON
-    const keysDataPath = path.join(__dirname, 'keys.json');
-    const keysData = JSON.parse(fs.readFileSync(keysDataPath, 'utf8'));
+exports.handler = async (event, context) => {
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-    // Verifica se o IP já solicitou uma chave nas últimas 24 horas
-    const lastKeyForIp = keysData.filter(k => k.ip === clientIp).sort((a, b) => new Date(b.Ativada) - new Date(a.Ativada))[0];
-    if (lastKeyForIp && (Date.now() - new Date(lastKeyForIp.Ativada).getTime()) < 24 * 60 * 60 * 1000) {
-        return {
-            statusCode: 429, 
-            body: JSON.stringify({ 
-                error: 'You can only generate one key per 24 hours.' 
-            })
-        };
-    }
+  let response = {};
 
-    const newKey = crypto.randomBytes(16).toString('hex');
+  try {
+    await client.connect();
+    
+    const collection = client.db(dbName).collection(collectionName);
+    
+    const newKey = {
+      Key: Math.random().toString(36).substr(2, 8).toUpperCase(),
+      Duration: "24h",
+      Used: "No",
+      Created: new Date().toISOString()
+    };
 
-    // Adiciona a nova chave aos dados
-    keysData.push({
-        Key: newKey,
-        Ativada: new Date().toISOString(),
-        Duracao: "24 hours",
-        Usada: false,
-        ip: clientIp // armazenar o IP para a verificação de 24 horas
-    });
+    const result = await collection.insertOne(newKey);
 
-    // Salva de volta no arquivo JSON
-    fs.writeFileSync(keysDataPath, JSON.stringify(keysData, null, 2));
+    const shortenedURLResponse = await axios.get(encurtaNetAPI);
+    
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Key generated successfully!", shortenedLink: shortenedURLResponse.data.shortenedUrl })
+    };
+  } catch (err) {
+    console.error('Error:', err);
+    response = {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Internal Server Error" })
+    };
+  } finally {
+    await client.close();
+  }
 
-    const apiUrl = `https://raihub.netlify.app/display-key/${newKey}`;
-    const customAlias = `RaiHub-${newKey.substring(0, 5)}`;
-
-    try {
-        const response = await axios.get(`https://encurta.net/api`, {
-            params: {
-                api: API_TOKEN,
-                url: apiUrl,
-                alias: customAlias,
-                format: 'text',
-                type: 0
-            }
-        });
-
-        // Redireciona o usuário para o link encurtado
-        return {
-            statusCode: 302,
-            headers: {
-                Location: response.data
-            },
-            body: ''
-        };
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: 'Error generating shortened link.'
-        };
-    }
+  return response;
 };
