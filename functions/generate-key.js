19a7151da036033740e8d49de03c7a6858bea55d
RaiProjct <111714032+RaiProjct@users.noreply.github.com>
2023-09-30 21:12:19 -0300

    update functions/generate-key.js

--- functions/generate-key.js
+++ functions/generate-key.js
@@ -12,7 +12,16 @@ exports.handler = async function(event, context) {
     const keysDataPath = path.join(__dirname, 'keys.json');
     const keysData = JSON.parse(fs.readFileSync(keysDataPath, 'utf8'));
 
-    // Verifica restrição de IP aqui, se necessário...
+    // Verifica se o IP já solicitou uma chave nas últimas 24 horas
+    const lastKeyForIp = keysData.filter(k => k.ip === clientIp).sort((a, b) => new Date(b.Ativada) - new Date(a.Ativada))[0];
+    if (lastKeyForIp && (Date.now() - new Date(lastKeyForIp.Ativada).getTime()) < 24 * 60 * 60 * 1000) {
+        return {
+            statusCode: 429, 
+            body: JSON.stringify({ 
+                error: 'You can only generate one key per 24 hours.' 
+            })
+        };
+    }
 
     const newKey = crypto.randomBytes(16).toString('hex');
 
@@ -20,12 +29,39 @@ exports.handler = async function(event, context) {
     keysData.push({
         Key: newKey,
         Ativada: new Date().toISOString(),
-        Duracao: "24 hours",  // Pode ser ajustado para mostrar um valor dinâmico
-        Usada: false
+        Duracao: "24 hours",
+        Usada: false,
+        ip: clientIp // armazenar o IP para a verificação de 24 horas
     });
 
     // Salva de volta no arquivo JSON
     fs.writeFileSync(keysDataPath, JSON.stringify(keysData, null, 2));
 
-    // ... (restante do código de geração do link encurtado)
+    const apiUrl = `https://raihub.netlify.app/use-key/${newKey}`;
+    const customAlias = `RaiHub-${newKey.substring(0, 5)}`;
+
+    try {
+        const response = await axios.get(`https://encurta.net/api`, {
+            params: {
+                api: API_TOKEN,
+                url: apiUrl,
+                alias: customAlias,
+                format: 'text',
+                type: 0
+            }
+        });
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                key: newKey, 
+                shortenedLink: response.data
+            })
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: 'Error generating shortened link.'
+        };
+    }
 };
